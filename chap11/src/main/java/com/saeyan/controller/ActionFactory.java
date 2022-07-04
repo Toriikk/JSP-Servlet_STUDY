2cd2f6da29554303cd38086911530e6d7b184fd8
Toriikk <dotorii0216@naver.com>
2022-07-04 21:44:56 +0900

    0704update

---
+++ chap11/src/main/java/com/saeyan/controller/ActionFactory.java
@@ -0,0 +1,38 @@
+package com.saeyan.controller;
+
+import com.saeyan.controller.action.Action;
+import com.saeyan.controller.action.BoardCheckPassFormAction;
+import com.saeyan.controller.action.BoardListAction;
+import com.saeyan.controller.action.BoardViewAction;
+import com.saeyan.controller.action.BoardWriteAction;
+import com.saeyan.controller.action.BoardWriteFormAction;
+
+public class ActionFactory {
+	private static ActionFactory instance = new ActionFactory();
+	
+	private ActionFactory() {
+		super();
+	}
+	
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+	
+	public Action getAction(String command) {
+		Action action = null;
+		System.out.println("ActionFactiory : " +  command);
+		
+		if(command.equals("board_list")) {
+			action = new BoardListAction();
+		} else if (command.equals("board_write_form")) {
+			action = new BoardWriteFormAction();
+		} else if (command.equals("board_write")) {
+			action = new BoardWriteAction();
+		} else if (command.equals("board_view")) {
+			action = new BoardViewAction();
+		} else if (command.equals("board_check_pass_form")) {
+			action = new BoardCheckPassFormAction();
+	}
+		return action;
+	}
+}
